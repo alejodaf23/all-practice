ab48f54c77179f9c756b80b7b00677a0ab8b99bc
Luis Miguel <luismiguel.ryan@gmail.com>
2018-09-07 12:16:08 -0400

    Primer ejercicio de javascript basic algorith

---
+++ JavaScript Algorithms and Data Structures Projects/Javascript Projects.js
@@ -0,0 +1,27 @@
+JavaScript Algorithms and Data Structures Projects: Palindrome Checker
+
+
+
+function palindrome(str) {
+  
+// Good luck!
+
+
+   let newStr = str.replace(/[\W_]/g, "").toLowerCase();
+
+   let reverse = newStr.split("").reverse().join("");
+
+
+
+	return newStr === reverse;
+
+ 
+
+
+}
+
+
+
+
+
+palindrome("eye"); 
