c58f2052cdd5ed8d176968ebd4fa41f477b60c30
Luis Miguel <luismiguel.ryan@gmail.com>
2018-09-11 12:28:31 -0400

    Modificacion de visaulizacion del codigo, queda igual.

--- JavaScript Algorithms and Data Structures Projects/Convert number in roman number.js
+++ JavaScript Algorithms and Data Structures Projects/Convert number in roman number.js
@@ -1,62 +1,30 @@
 function convertToRoman(num) {
- 
+  let roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
 
-let roman = { 
-   
-   M: 1000,
-    
-   CM: 900,
-    
-   D: 500,
-    
-   CD: 400,
-    
-   C: 100,
-    
-   XC:90,
-    
-   L: 50,
-    
-   XL: 40,
-     
-   X: 10,
-      
-   IX: 9,
-     
-   V: 5, 
-     
-   IV: 4,
-      
-   I: 1
-    
-};
-   
+  let ending = "";
 
-let ending = "";
-
-    
-
-for (let key in roman){
-    
+  for (let key in roman) {
     console.log(key);
-        
-while(num >= roman[key]){
-        
-    ending += key;
-         
-    num -= roman[key];
-         
-   console.log(num);
-  
-      }
-    
+    while (num >= roman[key]) {
+      ending += key;
+      num -= roman[key];
+      console.log(num);
     }
-
- 
+  }
   return ending;
-
 }
-
-
-
 convertToRoman(36);
